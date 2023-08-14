595d4e2f1031447f8cf26d1ade9155c79785dd08
Sadab Sifar <sadabsifar31@gmail.com>
2023-08-14 19:42:23 +0600

    updated all the pictures

--- model/faq.model.js
+++ model/faq.model.js
@@ -1,16 +1,29 @@
 exports.faqsData = [
   {
-    question: "What is your return policy?",
+    question:
+      "What is version control, and why is it important in software development?",
     answer:
-      "Our return policy allows customers to return items within 30 days of purchase.",
+      "Version control is a system that allows multiple developers to collaborate on a project while keeping track of changes. It helps in managing different versions of source code, coordinating work among team members, and providing a history of changes. Popular version control systems include Git and SVN.",
   },
   {
-    question: "How can I track my order?",
+    question:
+      "Explain the difference between agile and waterfall methodologies.",
     answer:
-      "You can track your order by logging into your account on our website.",
+      "Agile and waterfall are two different software development methodologies. Waterfall follows a sequential approach, where each phase is completed before moving to the next. Agile, on the other hand, is iterative and flexible, allowing for incremental development and adjustments based on feedback.",
   },
   {
-    question: "Do you offer international shipping?",
-    answer: "Yes, we offer international shipping to most countries.",
+    question: "What is the purpose of unit testing?",
+    answer:
+      "Unit testing is a practice in software development where individual components or units of code are tested in isolation. Its purpose is to ensure that each unit of the software functions correctly and meets the intended behavior. It helps identify bugs early in the development process.",
+  },
+  {
+    question: "Can you explain the SOLID principles in object-oriented design?",
+    answer:
+      "SOLID is an acronym for a set of five principles that help create maintainable and scalable software designs. The principles are Single Responsibility, Open/Closed, Liskov Substitution, Interface Segregation, and Dependency Inversion.",
+  },
+  {
+    question: "What is a RESTful API?",
+    answer:
+      "A RESTful API (Representational State Transfer) is an architectural style for designing networked applications. It uses HTTP methods (GET, POST, PUT, DELETE) to perform CRUD (Create, Read, Update, Delete) operations on resources. REST APIs are widely used for web services and communication between different systems.",
   },
 ];
